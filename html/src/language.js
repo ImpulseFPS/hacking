67766374057ac26e0a8a08f79002a3b2bf6bc153
ImpulseFPS <69686024+ImpulseFPS@users.noreply.github.com>
2022-01-24 16:53:19 +0100

    new

---
+++ html/src/language.js
@@ -0,0 +1,29 @@
+export default
+{
+    // langue française = 'FR'
+    // lengua española = 'ES'
+    // lingua italiana = 'IT'
+    // english language = 'EN'
+    // svenska språk = 'SE'
+    SELECTED_LANGUAGE : 'CRO',
+
+    LANGUAGES : ['EN', 'ES', 'IT', 'FR', 'SE', 'CRO'],
+    EN : {
+        COLORS : ['black', 'white', 'blue', 'red', 'yellow', 'orange', 'green', 'purple']
+    },
+    CRO : {
+        COLORS : ['crna', 'bijela', 'plava', 'crvena', 'zuta', 'narancasta', 'zelena', 'ljubicasta']
+    },
+    ES : {
+        COLORS : ['negro', 'blanco', 'azul', 'rojo', 'amarillo', 'naranja', 'verde', 'púrpura']
+    },
+    IT : {
+        COLORS : ['nero', 'bianco', 'blu', 'rosso', 'giallo', 'arancione', 'verde', 'viola']
+    },
+    FR : {
+        COLORS : ['noir', 'blanc', 'bleu', 'rouge', 'jaune', 'orange', 'vert', 'violet']
+    },
+    SE : {
+        COLORS : ['svart', 'vit', 'blå', 'röd', 'gul', 'orange', 'grön', 'lila']
+    }
+}
